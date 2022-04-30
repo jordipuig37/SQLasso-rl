4148b41a45513fd99c8fbef95e1795bfcdd8df01
Jose Pérez Cano <joseperez2000@hotmail.es>
2022-04-30 23:47:38 +0200

    rust

---
+++ rust/target/debug/build/libc-5f1d2576916917e2/build_script_build-5f1d2576916917e2.d
@@ -0,0 +1,5 @@
+/Users/joseperezcano/Desktop/HackUPC/rust/target/debug/build/libc-5f1d2576916917e2/build_script_build-5f1d2576916917e2: /Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.125/build.rs
+
+/Users/joseperezcano/Desktop/HackUPC/rust/target/debug/build/libc-5f1d2576916917e2/build_script_build-5f1d2576916917e2.d: /Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.125/build.rs
+
+/Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.125/build.rs:
---
+++ rust/target/debug/build/syn-6e51f8fc06be0ed8/build_script_build-6e51f8fc06be0ed8.d
@@ -0,0 +1,5 @@
+/Users/joseperezcano/Desktop/HackUPC/rust/target/debug/build/syn-6e51f8fc06be0ed8/build_script_build-6e51f8fc06be0ed8: /Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.92/build.rs
+
+/Users/joseperezcano/Desktop/HackUPC/rust/target/debug/build/syn-6e51f8fc06be0ed8/build_script_build-6e51f8fc06be0ed8.d: /Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.92/build.rs
+
+/Users/joseperezcano/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.92/build.rs:
